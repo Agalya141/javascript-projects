518149eda647c426a020a7fddc5075f6234bacaf
Agalya141 <agal4312@gmail.com>
2026-06-10 16:28:01 +0530

    Project Completed

--- script.js
+++ script.js
@@ -1,7 +1,14 @@
 let increment = document.getElementById("Enter");
+let saveButton = document.getElementById("save-btn")
 console.log(increment)
 let count = 0;
 function numbers(){
     count = count + 1;
     increment.innerText = count
+}
+function save(){
+total = count + " - "
+saveButton.textContent += total
+increment.textContent = 0
+count = 0
 }
